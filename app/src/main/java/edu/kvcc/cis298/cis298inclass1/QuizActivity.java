8e5fe39480819ffa1d95da45dc109fa7d1a9b356
Andy Cullen <thisisandycullen@gmail.com>
2015-09-23 16:25:51 -0400

    Add rest of coded for chapter 2

--- app/src/main/java/edu/kvcc/cis298/cis298inclass1/QuizActivity.java
+++ app/src/main/java/edu/kvcc/cis298/cis298inclass1/QuizActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
@@ -13,12 +14,76 @@ public class QuizActivity extends AppCompatActivity {
     //variables
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    //Variable for the question string
+    private TextView mQuestionTextView;
+
+    //The questions that will be used. It is an array of type Question, that contains 5 Q's.
+    //It has a hard coded array. In most apps, you would want your data to come from somewhere
+    //else (database/internet), without hardcoding.
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_lansing, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+    };
+
+    private int mCurrentIndex = 0;
+
+    //Private methods that will be used inside the OnCreate.
+    //I wrote these.
+
+    private void updateQuestion() {
+        //Get the Question instance stored at the mCurrentIndex of the QuestionBank array.
+        //Then call the getTextResId method (property) to return the integer value that
+        //points to the string resource in strings.xml that we want to use.
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+
+        //Assign the integer for the string resource to the textview so that the question
+        //text will display.
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue){
+        //create a boolean to represent the action answer of the current question we're on.
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        //declare an integer that will be a pointer to the string resource that will be used
+        //for the toast message.
+        int messageResId = 0;
+
+        //Compare the actual answer to the answer that was passed into this method. If they
+        //match, the message is correct, else it is incorrect. Assign the R in value to the
+        //messageResId
+        if (userPressedTrue == answerIsTrue){
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        //Make a toast, and use the messageResId for the message to show.
+        //Use the Toast class to print a message to the screen that will fade out after
+        //the duration listed as LENGTH_SHORT
+        //This method requires 3 parameters:
+        //1 Context, which usually be Activity.this,
+        //2 Message, which will usually be a string from strings .xml
+        //3 Length, which will be one of the predefined constants
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //This is the setup method for the app, provided by google
+        //This is called when the app launches
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        //BEGIN CODE I WROTE:-------------------------------------------------------------------
+
+        //Get a 'handle' to the textview in the layout
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
         //Fetch the widget control from the view, then cast and assign it to the class variable
         //we set up
@@ -30,15 +95,10 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Use the Toast class to print a message to the screen that will fade out after
-                //the duration listed as LENGTH_SHORT
-                //This method requires 3 parameters:
-                //1 Context, which usually be Activity.this,
-                //2 Message, which will usually be a string from strings .xml
-                //3 Length, which will be one of the predefined constants
-                Toast.makeText(QuizActivity.this,
-                                R.string.correct_toast,
-                                Toast.LENGTH_SHORT).show();
+                //Call the checkAnswer method and pass true. It will take in the bool value
+                //that was selected, and do the work of determining if the answer is correct.
+                //Either way, it will Toast the message to the screen.
+                checkAnswer(true);
             }
         });
 
@@ -46,11 +106,24 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+                //Call the checkAnswer method and pass false.
+                checkAnswer(false);
             }
         });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex +1) % mQuestionBank.length;
+                //This method is declared above. It does the work of changing questions in the array.
+                updateQuestion();
+            }
+
+        });
+
+        //This method is declared above. It does the work of changing questions in the array.
+        updateQuestion();
     }
 
     //These are methods that we did not write, but google provided.
